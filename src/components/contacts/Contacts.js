dd6bf5226a471ab29440f6a31747fcd62a2da677
td94 <quynhtrangdo94@gmail.com>
2018-11-23 12:22:47 -0500

    Actions and reducers

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -2,28 +2,6 @@ import React, { Component } from 'react';
 import Contact from './Contact';
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'john@gmail.com',
-        phone: '555-555-5555'
-      },
-      {
-        id: 2,
-        name: 'Karen Williams',
-        email: 'karen@gmail.com',
-        phone: '444-444-4444'
-      },
-      {
-        id: 3,
-        name: 'Henry Johnson',
-        email: 'henry@gmail.com',
-        phone: '333-333-333'
-      }
-    ]
-  };
 
   render() {
     const { contacts } = this.state;
