83c3830f23621f67cabe3a992702152941da95da
td94 <quynhtrangdo94@gmail.com>
2018-11-24 16:54:39 -0500

    Delete request dispatch

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -3,7 +3,7 @@ import TextInputGroup from '../layout/TextInputGroup';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addContact } from '../../actions/contactAction';
-import uuid from 'uuid';
+
 
 class AddContact extends Component {
   state = {
@@ -35,7 +35,7 @@ class AddContact extends Component {
     }
 
     const newContact = {
-      id: uuid(),
+  
       name,
       email,
       phone
